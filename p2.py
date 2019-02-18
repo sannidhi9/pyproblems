d0a01d9d99e49320e2931d90fb4674eff206387b
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-02-18 19:34:56 +0530

    p2 solution added

---
+++ p2.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/palindrome-number/
+#clink the above link to the problem description
+class Solution:
+    def isPalindrome(self, x: 'int') -> 'bool':
+        x=str(x)
+        i=0
+        i=int(i)
+        j=int(len(x)/2)
+        for i in range(j):
+            if x[int(i)]!=x[int(len(x)-1-i)]:
+                return False
+        return True
+        

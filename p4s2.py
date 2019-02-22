781282a2558227ae1f15203217a0ea374d116127
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-02-22 18:22:29 +0530

    Create p4s2.py

---
+++ p4s2.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution:
+    def removeDuplicates(self, nums: 'List[int]') -> 'int':
+        n=len(nums)-1
+        for i in range(n-1):
+            if nums[i]==nums[i+1]:
+                del nums[i]
+
+        return nums

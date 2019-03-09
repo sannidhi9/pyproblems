d698161042408525cc937c3821bb1362ccb88a42
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-09 22:35:15 +0530

    new changes made

--- p7.py
+++ p7.py
@@ -1,30 +1,15 @@
-#start
 #https://leetcode.com/problems/search-insert-position/
 #visit the above link to see problem  description
 class Solution:
-    def searchInsert(self, nums: 'List[int]', target: 'int') -> 'int':
-        if len(nums)==1 and target<nums[0]:
-            return 0
-        elif len(nums)==1 and target>nums[0]:
-            return 1
-        elif len(nums)==1 and target==nums[0]:
-            return 0
-            
-        for i in range(len(nums)-1):
-            if nums[i]==target:
-                return i
-            elif target<nums[i] and i==0:
-                return 0
-            elif target>nums[i] and target<nums[i+1]:
-                return i+1
-            
-            
-            if (i+1)==len(nums)-1 and target==nums[i+1]:
-                return i+1
-            elif (i+1)==len(nums)-1 and target<nums[i+1]:
-                return i
-            elif (i+1)==len(nums)-1 and target>nums[i+1]:
-                return i+2            
-
-
-#end
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        h=len(nums)-1
+        l=0
+        while l<=h :
+            mid=(h+l)//2
+            if nums[mid]==target:
+                return mid
+            elif target<nums[mid]:
+                h=mid-1
+            else:l=mid+1
+                
+        return l

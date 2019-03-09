bd8ab98f7bee71a1db27892ce45a2431bef5ba61
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-09 23:18:56 +0530

    new changes made

--- p8.py
+++ p8.py
@@ -1,10 +1,15 @@
-#start
 #https://leetcode.com/problems/length-of-last-word/
 #visit the above link to see problem description
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        x=s.split()
-        if len(s)!=0 and len(x)!=0:
-            return len(x[len(x)-1])
-        else:return 0
-#end
+        c=0
+        b=0
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==' ':
+                c=c+1
+            else:break
+        for i in range(len(s)-1-c,-1,-1):
+            if s[i]==' ':
+                break
+            b=b+1
+        return b

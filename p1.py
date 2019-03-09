f5cb54b488da26ddfd42df24172c81832f861da0
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-09 16:21:31 +0530

    new changes made

--- p1.py
+++ p1.py
@@ -1,10 +1,12 @@
-#start
 #https://leetcode.com/problems/two-sum/
 #click the above link to see the problem definition
 class Solution:
-    def twoSum(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        b={}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                    if nums[i]+nums[j]==target and i!=j:
-                        return(i,j)
-#end
+            c={nums[i]:i}
+            b.update(c)
+        for j in range(len(nums)-1):
+            if (target-nums[j]) in b and j!=b[target-nums[j]]:
+                return [j,b[target-nums[j]]]
+

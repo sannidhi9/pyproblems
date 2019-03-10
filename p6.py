182d2e8e9e7b3cc3cfe3882c1ec6af83b6dd8f0e
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-10 19:16:09 +0530

    new changes made

--- p6.py
+++ p6.py
@@ -1,8 +1,12 @@
-#start
 #https://leetcode.com/problems/implement-strstr/
 #visit the above link to see problem description
 class Solution:
-    def strStr(self, haystack: 'str', needle: 'str') -> 'int':
-            return haystack.find(needle)
-
-#end
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle=="":
+            return 0
+        else:
+            n=len(needle)
+            for i in range(len(haystack)):
+                if(haystack[i:i+n]==needle):
+                    return i
+            return -1

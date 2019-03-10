4e886a980894567f5a431ad12589c507586a1853
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-10 13:40:18 +0530

    new changes made

--- p5.py
+++ p5.py
@@ -1,12 +1,17 @@
-#start
 #https://leetcode.com/problems/remove-element/
 #visit the abve link to see the problem description
 class Solution:
     def removeElement(self, nums: 'List[int]', val: 'int') -> 'int':
-        n=len(nums)
-        for i in range(n-1,-1,-1):
+        count=0
+        if len(nums)==1 and nums[0]==val:
+            return 0
+        elif len(nums)==1 and nums[0]!=val:
+            return 1
+        for i in range(len(nums)-1,-1,-1):
             if nums[i]==val:
-                del nums[i]
-                
-        return len(nums)        
-#end
+                count=count+1
+                for j in range(i,len(nums)-1):
+                    temp=nums[j]
+                    nums[j]=nums[j+1]
+                    nums[j+1]=temp  
+        return len(nums)-count

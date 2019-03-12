54194a0664eaa7f8eb5cd707cb94b04c797e767d
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-12 18:05:19 +0530

    new changes made2

--- p2.py
+++ p2.py
@@ -2,9 +2,13 @@
 #clink the above link to the problem description
 class Solution:
     def isPalindrome(self, x: 'int') -> 'bool':
-        x=str(x)
-        j=len(x)//2
-        for i in range(j):
-            if x[int(i)]!=x[int(len(x)-1-i)]:
-                return False
-        return True
+        y,sum1=x,0
+        if x<0:return False
+        else:
+            while y!=0:
+                dig=y%10
+                sum1=sum1*10+dig
+                y=y//10
+        if sum1==x:
+            return True
+        else:return False

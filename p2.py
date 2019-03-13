28a4419246b33aa0abbe3315e09c399dd4c92a52
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-03-13 18:14:49 +0530

    new chages made3

--- p2.py
+++ p2.py
@@ -4,8 +4,8 @@ class Solution:
     def isPalindrome(self, x: 'int') -> 'bool':
         y,sum1=x,0
         if x<0:return False
-            while y!=0:
-                dig=y%10
-                sum1=sum1*10+dig
-                y=y//10
+        while y!=0:
+            dig=y%10
+            sum1=sum1*10+dig
+            y=y//10
         return sum1==x

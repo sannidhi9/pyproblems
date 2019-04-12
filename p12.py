d4fb203c45209bf226d5bbccdc1a65ed3db02d7e
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-12 20:14:54 +0530

    new changes made

--- p12.py
+++ p12.py
@@ -2,7 +2,19 @@
 #visit th eabove link to see problem definition
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(len(nums2)):
-            nums1[m]=nums2[i]
-            m=m+1
-        nums1.sort() 
+        k = m + n-1
+        i, j =m - 1,n - 1
+        while i>= 0 or j>= 0:
+            if i < 0:
+                nums1[k]=nums2[j]
+                j=j-1
+            elif j < 0:
+                nums1[k]=nums1[i]
+                i=i-1
+            elif nums1[i]>nums2[j]:
+                nums1[k]=nums1[i]
+                i=i-1
+            else: 
+                nums1[k]=nums2[j]
+                j=j-1
+            k=k-1

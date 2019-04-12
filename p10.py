5911960c0f770acb089cdb94eceedac4fe10f07b
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-12 19:10:57 +0530

    changes made

--- p10.py
+++ p10.py
@@ -2,20 +2,16 @@
 #visit the above link to view problem description
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        e,c,d,dic=[],0,[],{0:[0,0],1:[1,0],2:[0,1],3:[1,1]}
-        x=len(a)-len(b)
-        if x>0:
-            for j in range(x):
-                b="0"+b
-        elif x<0:
-            for k in range(abs(x)):
-                a="0"+a
-        for l in range(len(a)):
-            d=[0]+d        
-        for i in range(len(b)-1,-1,-1):
-            e=dic[int(a[i])+int(b[i])+c]
+        e,c,d=[],0,[]
+        d=[0]*max(len(a),len(b))+d
+        for i in range(max(len(a),len(b))-1,-1,-1):
+            while(len(a)!=len(b)):
+                if len(a)-len(b)>0:
+                    b=(len(a)-len(b))*'0'+b
+                elif len(a)-len(b)<0:
+                    a=abs(len(a)-len(b))*'0'+a
+            e=[(int(a[i])+int(b[i])+c)%2,(int(a[i])+int(b[i])+c)//2]
             d[i],c=e[0],e[1]
             if c==1 and i==0:
                 d=[1]+d
-        res ="".join(map(str, d))         
-        return res
+        return "".join(map(str, d))

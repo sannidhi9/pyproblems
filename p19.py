f8004d0e6c73a0c35aeee24e621ff5ed01003ce9
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-15 22:03:34 +0530

    new changes made

--- p19.py
+++ p19.py
@@ -1,6 +1,6 @@
 #https://leetcode.com/problems/rotate-array/submissions/
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        k=k%len(nums)
-        for i in range(k):
-            nums.insert(0,nums.pop())
+        nums_copy = nums.copy()
+        for i in range(0, len(nums)):
+            nums[(i+k) % len(nums)] = nums_copy[i]

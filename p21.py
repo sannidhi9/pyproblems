ae3028b9ff3e46c4f487f9cc6d07061328c784a3
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-15 22:47:42 +0530

    new changes made

--- p21.py
+++ p21.py
@@ -3,10 +3,10 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         i,r,s,t=0,0,str(n),[]
         while(i<len(s)):
-            r=r+int(s[i])**2
-            i=i+1
+            r+=int(s[i])**2
+            i+=1
             if i==len(s):
                 if r==1:return True
-                elif r in t:return False
-                t=t+[r]
+                elif r in set(t):return False
+                t.append(r)
                 s,i,r=str(r),0,0

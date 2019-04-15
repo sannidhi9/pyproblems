d0dafb817c7b165d92cc6364b37cbed506b1fcfd
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-15 19:17:51 +0530

    new changes made

--- p17.py
+++ p17.py
@@ -1,8 +1,8 @@
 #https://leetcode.com/problems/majority-element/
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d=collections.Counter(nums)
-        b=max(d.values())
-        key_list = list(d.keys()) 
-        val_list = list(d.values()) 
-        return (key_list[val_list.index(b)])
+        d={}
+        for i in range(len(nums)):
+            if nums[i] not in list(d.values()):
+                d.update({nums.count(nums[i]):nums[i]})
+        return d[max(list(d.keys()))]

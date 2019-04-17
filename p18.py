c4a8fca2c9dcaf343da5ae1907ba224e09db6f0f
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-04-17 11:30:27 +0530

    new changes made

--- p18.py
+++ p18.py
@@ -1,10 +1,9 @@
 #https://leetcode.com/problems/excel-sheet-column-number/
 class Solution:
     def titleToNumber(self, s: str) -> int:
-        d={'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
         if len(s)==1:
-            return d[s[0]]
-        r=d[s[0]]
+            return ord(s[0])-64
+        r=ord(s[0])-64
         for i in range(len(s)-1):
-            r=r*26+d[s[i+1]]
+            r=r*26+ord(s[i+1])-64
         return r
